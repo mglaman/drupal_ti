cad701453f0ec05414e7d09a14bd037842a5bec8
Matt Glaman <nmd.matt@gmail.com>
2017-02-23 23:16:22 -0600

    MOAR DEBUG

--- runners/phpunit-core/script.sh
+++ runners/phpunit-core/script.sh
@@ -12,8 +12,11 @@ ARGS=( $DRUPAL_TI_PHPUNIT_ARGS )
 echo "DEBUGGING"
 echo $MODULE_DIR
 echo "$MODULE_DIR/$DRUPAL_TI_MODULE_NAME/$DRUPAL_TI_PHPUNIT_CORE_SRC_DIRECTORY"
+echo ${ARGS[@]}
+ls ../vendor/bin
 echo "END DEBUGGING"
 
 # Run core tests
 cd core
+echo "WHAT."
 ../vendor/bin/phpunit "${ARGS[@]}" "$MODULE_DIR/$DRUPAL_TI_MODULE_NAME/$DRUPAL_TI_PHPUNIT_CORE_SRC_DIRECTORY"
